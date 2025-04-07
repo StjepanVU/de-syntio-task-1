ba28e7daa61b1be8303bdb432104f7464e85453f
Stjepan <stjepan.milicic@gmail.com>
2025-04-07 17:41:04 +0200

    port fix

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -62,4 +62,4 @@ def store_request(transformed_data):
     return storage_status
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
